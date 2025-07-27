0638bb2a4e571760e6a05663deed6a1ac32de8e3
rohobits <fostermartin67@gmail.com>
2025-07-27 17:03:03 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="space-y-6">
             <img 
-              src="/assets/Summit_Sites_Logo_Optimized.jpg" 
+              src="assets/Summit_Sites_Logo_Optimized.jpg" 
               alt="Summit Sites Logo" 
               className="w-48 h-auto brightness-0 invert"
             />
