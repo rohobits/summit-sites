15fd7effe2d0892af1bb39ce43effae415f8acb4
rohobits <fostermartin67@gmail.com>
2025-07-27 14:51:29 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/summit-sites/", // 👈 required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
